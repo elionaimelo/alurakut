715796f5e6c2f001c04595fd01a769a4a58efbce
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-07-17 17:11:27 -0300

    Versionando

---
+++ pages/api/comunidades.js
@@ -0,0 +1,30 @@
+import { SiteClient } from 'datocms-client';
+
+export default async function recebedorDeRequests(request, response) {
+    response => response.json()
+    if(request.method === 'POST') {
+        const TOKEN = '012975c352c20062dd096e715e0b51';
+        const client = new SiteClient(TOKEN);
+        
+        // Validar os dados, antes de sair cadastrando
+        const registroCriado = await client.items.create({
+            itemType: "976011", // ID do Model de "Communities" criado pelo Dato
+            ...request.body,
+            // title: "Comunidade de Teste",
+            // imageUrl: "https://github.com/omariosouto.png",
+            // creatorSlug: "omariosouto"
+        })
+    
+        console.log(registroCriado);
+    
+        response.json({
+            dados: 'Algum dado qualquer',
+            registroCriado: registroCriado,
+        })
+        return;
+    }
+
+    response.status(404).json({
+        message: 'Ainda não temos nada no GET, mas no POST tem!'
+    })
+}
