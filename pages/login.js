bc010a883411d4943170814e99c7240574dd1c46
Elionai Melo <elionai.melo@lais.huol.ufrn.br>
2021-07-18 11:03:28 -0300

    Validação de campo

--- pages/login.js
+++ pages/login.js
@@ -1,73 +1,87 @@
-import React from 'react';
+import React from "react";
 // Hook do NextJS
-import { useRouter } from 'next/router';
-import nookies from 'nookies';
-import { useForm } from 'react-hook-form';
+import { useRouter } from "next/router";
+import nookies from "nookies";
+import { useForm } from "react-hook-form";
 
 export default function LoginScreen() {
   const router = useRouter();
-  const [githubUser, setGithubUser] = React.useState('');
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [githubUser, setGithubUser] = React.useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const onSubmit = data => {
-    fetch('https://alurakut.vercel.app/api/login', {
-        method: 'POST',
-        headers: {
-           'Content-Type': 'application/json'  
-        },
-        body: JSON.stringify({ githubUser: data })
-    })
-    .then(async (respostaDoServer) => {
-        const dadosDaResposta = await respostaDoServer.json()
-        const token = dadosDaResposta.token;
-        nookies.set(null, 'USER_TOKEN', token, {
-            path: '/',
-            maxAge: 86400 * 7 
-        })
-        router.push('/')
-    })
+  function onSubmit() {
+
+    fetch("https://alurakut.vercel.app/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ githubUser: githubUser }),
+    }).then(async (respostaDoServer) => {
+      const dadosDaResposta = await respostaDoServer.json();
+      const token = dadosDaResposta.token;
+      nookies.set(null, "USER_TOKEN", token, {
+        path: "/",
+        maxAge: 86400 * 7,
+      });
+      router.push("/");
+    });
   }
-  
-
 
   return (
-    <main style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <main
+      style={{
+        display: "flex",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
       <div className="loginScreen">
         <section className="logoArea">
           <img src="https://alurakut.vercel.app/logo.svg" />
 
-          <p><strong>Conecte-se</strong> aos seus amigos e familiares usando recados e mensagens instantâneas</p>
-          <p><strong>Conheça</strong> novas pessoas através de amigos de seus amigos e comunidades</p>
-          <p><strong>Compartilhe</strong> seus vídeos, fotos e paixões em um só lugar</p>
+          <p>
+            <strong>Conecte-se</strong> aos seus amigos e familiares usando
+            recados e mensagens instantâneas
+          </p>
+          <p>
+            <strong>Conheça</strong> novas pessoas através de amigos de seus
+            amigos e comunidades
+          </p>
+          <p>
+            <strong>Compartilhe</strong> seus vídeos, fotos e paixões em um só
+            lugar
+          </p>
         </section>
 
         <section className="formArea">
           <form className="box" onSubmit={handleSubmit(onSubmit)}>
             <p>
               Acesse agora mesmo com seu usuário do <strong>GitHub</strong>!
-          </p>
+            </p>
             <input
-                {...register("usuario", { required: true })} 
-                placeholder="Usuário"
-                value={githubUser}
-                onChange={(evento) => {
-                    setGithubUser(evento.target.value)
-                }}
+              {...register("usuario", { required: true })}
+              placeholder="Usuário"
+              value={githubUser}
+              onChange={(evento) => {
+                setGithubUser(evento.target.value);
+              }}
             />
             {errors.usuario && <span>Este campo é obrigatório</span>}
-            <br/>
-            <button type="submit">
-              Login
-            </button>
+            <br />
+            <button type="submit">Login</button>
           </form>
 
           <footer className="box">
             <p>
               Ainda não é membro? <br />
               <a href="/login">
-                <strong>
-                  ENTRAR JÁ
-              </strong>
+                <strong>ENTRAR JÁ</strong>
               </a>
             </p>
           </footer>
@@ -75,10 +89,12 @@ export default function LoginScreen() {
 
         <footer className="footerArea">
           <p>
-            © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> - <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> - <a href="/">Termos</a> - <a href="/">Contato</a>
+            © 2021 alura.com.br - <a href="/">Sobre o Orkut.br</a> -{" "}
+            <a href="/">Centro de segurança</a> - <a href="/">Privacidade</a> -{" "}
+            <a href="/">Termos</a> - <a href="/">Contato</a>
           </p>
         </footer>
       </div>
     </main>
-  )
-} 
+  );
+}
